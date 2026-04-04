36783fdae2929fe7de6f9a796dca3504cefc2ecd
Sanyamkk <kotharisanyam6@gmail.com>
2026-04-04 22:07:05 +0530

    removing my password

--- lec4/config/db.js
+++ lec4/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await  mongoose.connect("mongodb+srv://Sanyamk48:Sanyam@cluster0.0iy6lho.mongodb.net/hlo");
+        await  mongoose.connect("mongodb+srv://<your_Password>:Sanyam@cluster0.0iy6lho.mongodb.net/hlo");
         console.log("DB connected successfully");
     } catch (err) {
         console.log("DB connection error:", err);
