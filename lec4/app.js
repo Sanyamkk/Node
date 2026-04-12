127ab2297cbb418503e64842599dc332fb588104
Sanyamkk <kotharisanyam6@gmail.com>
2026-04-12 15:48:08 +0530

    creating diffrent api calls

--- lec4/app.js
+++ lec4/app.js
@@ -2,21 +2,89 @@ const express=require('express');
 const { connectDB } = require('./config/db');
 const { UserModel } = require('./models/user');
 
+
 const app=express();
 
+
+app.use(express.json()); // this will parse the incoming request body and make it available in req.body
+
+
+// find all users
+app.get("/feed" , async (req , res)=>{
+    try{
+        const user=await UserModel.find({}) // this will return an array of all the users in the database
+
+        if(user.length===0){
+            res.status(404).send("user not found");
+        }
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("error in finding user");
+    }
+});
+
+
+//  find user by email
+app.get("/user" , async (req , res)=>{
+    const email=req.body.email;
+    try{
+        const user=await UserModel.find({email:email}) // this will return an array of users with the given email
+        if(user.length===0){
+            res.status(404).send("user not found");
+        }
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("error in finding user");
+    }
+});
+
+
+// delete user by email
+app.delete("/user" , async(req , res)=>{
+    const userId=req.body.userId;
+    try{
+        const user=await UserModel.findByIdAndDelete(userId); // this will delete the user with the given userId
+        res.send("user deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("error in deleting user");
+    }
+});
+
+
+// update user by email
+app.patch("/user" , async(req , res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+
+        await UserModel.findByIdAndUpdate(userId , data); // this will update the user with the given userId and data
+        console.log(data);
+        res.send("user updated successfully");
+    }
+    catch(err){
+        res.status(400).send("error in updating user");
+    }
+});
+// signup route
 app.post("/signup" , async (req , res)=>{
     const userobj={
-        firstName:"Sanyam",
-        lastName:"Kothari",
-        email:"kotharisanyam6@gamil.com",
-        password:"sanyam@123",
-        age:21, 
-        gender:"male",  
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        password:req.body.password,
+        age:req.body.age,
+        gender:req.body.gender,
     }
+    console.log(req.body);
     const user=new UserModel(userobj); // we need to make one new instance of the model to save the data in the database
     await user.save() // this will save the data in the database
     res.send("signup successful");
 })
+
+// connect to the database and start the server
 connectDB()
 .then(()=>{
 
--- lec4/config/db.js
+++ lec4/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+
 const connectDB = async() => {
-    await mongoose.connect("mongodb+srv://<your_passwaed>:Sanyam@cluster0.0iy6lho.mongodb.net/hlo");
+    await mongoose.connect("mongodb+srv://<your_passward>:Sanyam@cluster0.0iy6lho.mongodb.net/hlo");
 
 };
 
