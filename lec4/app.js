151efa8d1d5f1c494c7398c3ba1464e5f598f88a
Sanyamkk <kotharisanyam6@gmail.com>
2026-04-12 08:49:06 +0530

    lec 4

--- lec4/app.js
+++ lec4/app.js
@@ -1,31 +1,15 @@
-const express = require('express');
-const {connectDB} = require('./config/db');
-const { UserModel } = require('./models/user');
+const express=require('express');
+const { connectDB } = require('./config/db');
 
+const app=express();
 
-connectDB();
-const app = express();
+connectDB().then(()=>{
 
-app.post("/signup", async (req, res) => {
-    try {
-        const userObj = {
-            firstName: "Sanyam",
-            lastName: "Kothari",
-            emailId: "kotharisanyam6@gmail.com",
-            password: "abc@123",
-            age: 22,
-            gender: "male"
-        };
-
-        const user = new UserModel(userObj);
-        await user.save();
-
-        res.send("added successfully");
-    } catch (err) {
-        res.status(500).send("Error: " + err.message);
-    }
+    console.log("connected to db");
+    app.listen( 3001 , ()=>{
+    console.log("server is running on port 3001");
+});
+}).catch((err)=>{
+    console.log(err);
 });
 
-app.listen(3001, () => {
-    console.log("server running on port 3001");
-});
--- lec4/config/db.js
+++ lec4/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await  mongoose.connect("mongodb+srv://<your_Password>:Sanyam@cluster0.0iy6lho.mongodb.net/hlo");
-        console.log("DB connected successfully");
-    } catch (err) {
-        console.log("DB connection error:", err);
-    }
+const connectDB = async() => {
+    await mongoose.connect("mongodb+srv://Sanyamk48:Sanyam@cluster0.0iy6lho.mongodb.net/hlo");
+
 };
 
-module.exports = {connectDB};
+
+module.exports = { connectDB };
